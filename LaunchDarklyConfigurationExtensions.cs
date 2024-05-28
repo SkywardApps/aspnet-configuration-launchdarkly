b4bfd634333c47ae5fc4f9c34945408ec299ffe9
Nicholas Elliott <nelliott@skywardapps.com>
2024-05-28 14:59:45 -0400

    LuanchDarkly provider

---
+++ LaunchDarklyConfigurationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+#nullable enable
+namespace Skyward.Aspnet.Configuration
+{
+    public static class LaunchDarklyConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddLaunchDarklyConfiguration(
+            this IConfigurationBuilder builder, string sdkKey, string prefix = "configure-backend-")
+        {
+            return builder.Add(new LaunchDarklyConfigurationSource(sdkKey, prefix));
+        }
+    }
+}
---
+++ LaunchDarklyConfigurationSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+#nullable enable
+namespace Skyward.Aspnet.Configuration
+{
+    class LaunchDarklyConfigurationSource : IConfigurationSource
+    {
+        private readonly string _sdkKey;
+        private readonly string _prefix;
+
+        public LaunchDarklyConfigurationSource(string sdkKey, string prefix)
+        {
+            _sdkKey = sdkKey ?? throw new ArgumentNullException(nameof(sdkKey));
+            _prefix = prefix ?? throw new ArgumentNullException(nameof(prefix));
+        }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new LaunchDarklyConfigurationProvider(_sdkKey, _prefix);
+        }
+    }
+}
